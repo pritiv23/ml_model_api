e87b211d445f7f12849f216451efc51a1dbb6a77
Priti verma <pritiverma@Pritis-MacBook-Air.local>
2026-05-26 01:41:09 +0530

    final render fix

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 import pickle
+import os
 
 app = FastAPI()
 
-with open("model.pkl", "rb") as f:
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "model.pkl")
+
+with open(model_path, "rb") as f:
     model = pickle.load(f)
 
 @app.get("/")
